030019af658f1dd5b2dd62e2f041cd46082de426
Dmytro Riazanov <dmytro.riazanov@globallogic.com>
2017-07-23 11:02:04 -0700

    ignore log and sqlite files. refactor specs and methods

--- lib/recommender.rb
+++ lib/recommender.rb
@@ -5,11 +5,15 @@ class Recommender
 
   def by_category
     Item.includes(:categories)
-      .where(categories: { id: @user.category_ids.uniq }) - @user.items
+      .where(categories: { id: @user.category_ids.uniq })
+      .where.not(id: @user.item_ids)
   end
 
   def by_user
-    users = User.includes(:items).where(items: { id: @user.item_ids.uniq }) - [@user]
-    Item.includes(:users).where(users: { id: users.map(&:id) }) - @user.items
+    user_ids = User.includes(:items)
+      .where(items: { id: @user.item_ids.uniq })
+      .where.not(id: @user.id).pluck(:id)
+    Item.includes(:users).where(users: { id: user_ids })
+      .where.not(id: @user.item_ids)
   end
 end
--- spec/recommender_spec.rb
+++ spec/recommender_spec.rb
@@ -3,30 +3,39 @@ require 'recommender'
 
 RSpec.describe Recommender, '#general' do
   context '#by_category' do
+    let!(:user) { create(:user) }
+    let!(:nike_cat) { create(:category, name: 'Nike') }
+    let!(:bball_cat) { create(:category, name: 'Basketball') }
+    let!(:shoes_cat) { create(:category, name: 'Shoes') }
+    let!(:cloth_cat) { create(:category, name: 'Clothing') }
+    let!(:balls_cat) { create(:category, name: 'Balls') }
+    let!(:adidas_cat) { create(:category, name: 'Addidas') }
+    let!(:accs_cat) { create(:category, name: 'Accessories') }
+
+    let!(:item1) { create(:item, name: 'Nike Dunks',
+      categories: [shoes_cat, bball_cat, nike_cat],
+      users: [user]) }
+    let!(:item2) { create(:item, name: 'Nike Street Basketball',
+      categories: [balls_cat, bball_cat, nike_cat]) }
+    let!(:item3) { create(:item, name: 'Addidas Jersey',
+      categories: [cloth_cat, bball_cat, adidas_cat]) }
+    let!(:item4) { create(:item, name: 'Golf bag', categories: [accs_cat]) }
+
     it 'recommends nothing (0) if there is no items in store' do
       user = create(:user)
       expect(Recommender.new(user).by_category.count).to be 0
     end
 
-    it 'recommends 2 items from similar categories' do
-      user = create(:user)
-      nike_cat = create(:category, name: 'Nike')
-      bball_cat = create(:category, name: 'Basketball')
-      shoes_cat = create(:category, name: 'Shoes')
-      cloth_cat = create(:category, name: 'Clothing')
-      balls_cat = create(:category, name: 'Balls')
-      adidas_cat = create(:category, name: 'Addidas')
-      accs_cat = create(:category, name: 'Accessories')
-
-      item1 = create(:item, name: 'Nike Dunks',
-        categories: [shoes_cat, bball_cat, nike_cat],
-        users: [user])
-      item2 = create(:item, name: 'Nike Street Basketball', categories: [balls_cat, bball_cat, nike_cat])
-      item3 = create(:item, name: 'Addidas Jersey', categories: [cloth_cat, bball_cat, adidas_cat])
-      item4 = create(:item, name: 'Golf bag', categories: [accs_cat])
-      expect(Recommender.new(user).by_category.count).to be 2
-      expect(Recommender.new(user).by_category).to include(item2, item3)
+    it 'excludes Nike Dunks and Golf bag' do
       expect(Recommender.new(user).by_category).to_not include(item1, item4)
+    end
+
+    it 'includes Nike Street Basketball and Addidas Jersey' do
+      expect(Recommender.new(user).by_category).to include(item2, item3)
+    end
+
+    it 'recommends 2 items from similar categories' do
+      expect(Recommender.new(user).by_category.count).to be 2
     end
   end
 
@@ -52,6 +61,10 @@ RSpec.describe Recommender, '#general' do
 
     it 'recomends Nike Street Basketball item for John' do
       expect(Recommender.new(john).by_user).to include(item2)
+    end
+
+    it 'excludes already bought items for John' do
+      expect(Recommender.new(john).by_user).to_not include(item1)
     end
   end
 end
